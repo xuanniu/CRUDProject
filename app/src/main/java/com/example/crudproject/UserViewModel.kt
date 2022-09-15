e373ed37b00445c137d16a33f61a81e8c33ac64c
xuanniu <xuanniu@gmail.com>
2022-09-15 16:27:06 -0700

    Added basic UI for crud operations

--- app/src/main/java/com/example/crudproject/UserViewModel.kt
+++ app/src/main/java/com/example/crudproject/UserViewModel.kt
@@ -1,4 +1,34 @@
 package com.example.crudproject
 
-class UserViewModel {
+import androidx.lifecycle.MutableLiveData
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class UserViewModel(val repo: UserRepository) {
+
+    var userList = MutableLiveData<Map<String,User>>()
+
+    fun getAllUsers() {
+        CoroutineScope(Dispatchers.IO).launch {
+            var res = repo.getAllUsers()
+            if(res.isSuccessful) {
+                userList.postValue(res.body())
+            }
+        }
+    }
+
+    fun deleteUser(id: String) {
+        CoroutineScope(Dispatchers.IO).launch {
+            repo.deleteUser(id)
+            getAllUsers()
+        }
+    }
+
+    fun createUser(user: User) {
+        CoroutineScope(Dispatchers.IO).launch {
+            repo.createUser(user)
+            getAllUsers()
+        }
+    }
 }
